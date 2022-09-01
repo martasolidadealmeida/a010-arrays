e11c86c4a7a100b24ef457e2372ac935de393e2c
martasolidadealmeida <martamarcio123@gmail.com>
2022-09-01 16:53:41 -0300

    exercicios-arrays completos

---
+++ exercicio1/script.js
@@ -0,0 +1,17 @@
+//No primeiro exercício vamos praticar a criação de arrays. Crie 3 arrays diferentes:
+
+//1) O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica;
+const arrayNumero = [6,20,8,35,1]
+console.log(arrayNumero)
+
+
+//2) O segundo array deve possuir apenas **strings**;
+const arrayStrings = ["bolo" , "pizza" , "sorvete"]
+console.log(arrayStrings)
+
+
+// 3) O terceiro array deve possuir **números**, **strings** e **booleanos**.
+const mista = [20 , "olá", false]
+console.log(mista)
+
+
